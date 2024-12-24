2654cb0a573bceccecb227dd35f762fa745b78fb
ZZZank <3410764033@qq.com>
2024-12-24 09:43:29 +0800

    block entities registration

--- common/src/main/java/com/ichphilipp/logicchips/utils/RegistryMgr.java
+++ common/src/main/java/com/ichphilipp/logicchips/utils/RegistryMgr.java
@@ -1,14 +1,19 @@
 package com.ichphilipp.logicchips.utils;
 
+import com.google.common.collect.ImmutableSet;
 import com.ichphilipp.logicchips.LogicChips;
 import com.ichphilipp.logicchips.blocks.LogicChipsBlock;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.Util;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.util.datafix.fixes.References;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +29,8 @@ public class RegistryMgr {
         .create(LogicChips.MOD_ID, Registries.ITEM);
     public static final DeferredRegister<Block> BLOCK_REG = DeferredRegister
         .create(LogicChips.MOD_ID, Registries.BLOCK);
+    public static final DeferredRegister<BlockEntityType<?>> BE_TYPE_REG = DeferredRegister
+        .create(LogicChips.MOD_ID, Registries.BLOCK_ENTITY_TYPE);
 
     public static <T extends Item> RegistrySupplier<T> registerItem(String name, Supplier<T> item) {
         return ITEM_REG.register(name, item);
@@ -35,6 +42,21 @@ public class RegistryMgr {
 
     public static <T extends Block> RegistrySupplier<BlockItem> registerBlockItem(String name, RegistrySupplier<T> block) {
         return ITEM_REG.register(name, () -> new BlockItem(block.get(), LogicChips.DEFAULT_ITEM_PROP));
+    }
+
+    public static <T extends BlockEntity> RegistrySupplier<BlockEntityType<T>> registerBE(
+        String name,
+        BlockEntityType.BlockEntitySupplier<T> getterBE,
+        Block... validBlocks
+    ) {
+        return BE_TYPE_REG.register(
+            name,
+            () -> new BlockEntityType<>(
+                getterBE,
+                ImmutableSet.copyOf(validBlocks),
+                Util.fetchChoiceType(References.BLOCK_ENTITY, name)
+            )
+        );
     }
 
     public static void init() {
